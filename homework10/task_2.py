95da74a80ddf23cb5cebecde334db1d218dd1658
mikhail <mishaprizby@gmail.com>
2022-11-30 20:20:50 +0300

    task2 middle

---
+++ homework10/task_2.py
@@ -0,0 +1,27 @@
+import task_1 as t1
+import time
+
+class Truck(t1.Auto):
+    max_load = 30
+
+    def __init__(self, brand: str, age: int, mark: str) -> None:
+        super().__init__(brand, age, mark)
+
+    def drive(self):
+        print('Attention!')
+        return super().drive()
+    
+    def load(self):
+        time.sleep(1)
+        print('load')
+        time.sleep(1)
+
+class Sedan(t1.Auto):
+    max_speed =100
+    def __init__(self, brand: str, age: int, mark: str) -> None:
+        super().__init__(brand, age, mark)
+
+    def drive(self):
+        super().drive()
+        print(f'max speed of sedan {self.brand} {self.mark} is {Sedan.max_speed}')
+
