e853a0627a18687011ab477e255885df7d1abde5
mikhail <mishaprizby@gmail.com>
2023-01-30 01:05:07 +0300

    урок 23,24

--- homework23/game_shop/games/models.py
+++ homework23/game_shop/games/models.py
@@ -2,12 +2,16 @@ from django.db import models
 
 
 # Create your models here.
-class Category(models.Model):
-
-    title = models.CharField(max_length=50, verbose_name="Category title" )
-    description = models.TextField(max_length=150, verbose_name= "Category description")
-    is_active = models.BooleanField(verbose_name='Category is active')
+class ShopInfoMixin(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Title" )
+    description = models.TextField( verbose_name= "Description")
+    is_active = models.BooleanField(verbose_name='is active')
     slug = models.SlugField(unique=True, verbose_name="URL")
+
+    class Meta:
+        abstract = True
+class Category(ShopInfoMixin):
+
     games_amount = models.IntegerField(default=0, verbose_name='amount')
 
     class Meta:
@@ -17,16 +21,12 @@ class Category(models.Model):
     def __str__ (self):
         return(f'{self.title}')
 
-class Game(models.Model):
+class Game(ShopInfoMixin):
 
-    name = models.CharField(max_length=50, verbose_name='Game Name')
-    description = models.TextField(verbose_name='Game Description')
-    is_active = models.BooleanField(verbose_name='Game is active?')
     pub_date = models.DateTimeField(auto_now_add=True, verbose_name='publish date')
     release_date_at = models.DateField(verbose_name='Release date')
     price = models.DecimalField(verbose_name='Game Prise', max_digits=8, decimal_places=2)
     category = models.ForeignKey(Category, verbose_name=("Game Category"), on_delete=models.SET_NULL, null=True)
-    slug = models.SlugField(unique=True, verbose_name="URL")
     game_image = models.ImageField(upload_to='images/')
     
 
@@ -35,7 +35,7 @@ class Game(models.Model):
         verbose_name_plural = 'Games'
 
     def __str__(self):
-        return (f"{self.name}")
+        return (f"{self.title}")
 
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
